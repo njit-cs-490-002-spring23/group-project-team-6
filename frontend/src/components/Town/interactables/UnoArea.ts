7fed5db63a664108212e863bf167227d208e0136
Alex Joseph <AlexPJoseph80@gmail.com>
2023-11-05 02:45:15 -0500

    Added a file for our application

---
+++ frontend/src/components/Town/interactables/UnoArea.ts
@@ -0,0 +1,5 @@
+import ConversationAreaController from '../../../classes/ConversationAreaController';
+import TownController from '../../../classes/TownController';
+import { BoundingBox } from '../../../types/CoveyTownSocket';
+import Interactable, { KnownInteractableTypes } from '../Interactable';
+import TownGameScene from '../TownGameScene';
