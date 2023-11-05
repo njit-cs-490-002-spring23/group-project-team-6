252c7ac60ff7134ecea7638f94eab866911b6b58
Alex Joseph <AlexPJoseph80@gmail.com>
2023-11-05 15:56:06 -0500

    made files for task 9 Defining Cards Heirarchy

--- frontend/src/components/Town/interactables/UnoArea.ts
+++ frontend/src/components/Town/interactables/UnoArea.ts
@@ -2,4 +2,30 @@ import ConversationAreaController from '../../../classes/ConversationAreaControl
 import TownController from '../../../classes/TownController';
 import { BoundingBox } from '../../../types/CoveyTownSocket';
 import Interactable, { KnownInteractableTypes } from '../Interactable';
-import TownGameScene from '../TownGameScene';
+// import TownGameScene from '../TownGameScene';
+// export default class Uno extends Interactable {
+//     private _topicTextOrUndefined?: Phaser.GameObjects.Text;
+  
+//     private _infoTextBox?: Phaser.GameObjects.Text;
+  
+//     private _conversationArea?: ConversationAreaController;
+  
+//     private _townController: TownController;
+  
+//     constructor(scene: TownGameScene) {
+//       super(scene);
+//       this._townController = scene.coveyTownController;
+//       this.setTintFill();
+//       this.setAlpha(0.3);
+//       this._townController.addListener('conversationAreasChanged', this._updateConversationAreas);
+//     }
+  
+//     private get _topicText() {
+//       const ret = this._topicTextOrUndefined;
+//       if (!ret) {
+//         throw new Error('Expected topic text to be defined');
+//       }
+//       return ret;
+//     }
+//   }
+  
