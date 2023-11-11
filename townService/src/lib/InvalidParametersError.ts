1dbfe7de8a1e8aa6847af24247893c7ce7939be6
Samuel Levshteyn <slevshteyn@gmail.com>
2023-11-10 20:14:46 -0500

    Updated linter settings, InvalidParameters list. Implemented UnoGame.ts join and leave methods.
    Changed linter settings to fix current lint errors while compiling - can be changed later. Created new invalid parameters to call from. Updated UnoGame to allow players to join and leave game. Join method sets a players left and right players when joining and sets status to waiting until 6 players are reached, leave method gets rid of player from player list.

--- townService/src/lib/InvalidParametersError.ts
+++ townService/src/lib/InvalidParametersError.ts
@@ -1,4 +1,9 @@
 export const PLAYER_NOT_IN_GAME_MESSAGE = 'Player is not in this game';
+export const PLAYER_ALREADY_IN_GAME_MESSAGE = 'Player is already in this game';
+export const GAME_FULL_MESSAGE = 'Game is full';
+export const GAME_NOT_IN_PROGRESS_MESSAGE = 'Game as either ended or nto started yet';
+export const MOVE_NOT_YOUR_TURN_MESSAGE = 'It is not your turn';
+
 export default class InvalidParametersError extends Error {
   public message: string;
 
